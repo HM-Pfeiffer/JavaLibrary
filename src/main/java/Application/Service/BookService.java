8d5c07717a21b72962db87fc6bdfe2c721c4da5f
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-04-01 11:53:09 -0400

    Update BookService.java

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 /**
  * The purpose of a Service class is to contain "business logic" that sits between the web layer (controller) and
- * persistence layer (DAO). That means that the Service class performs tasks that aren't done through the web or
- * SQL: programming tasks like checking that the input is valid, conducting additional security checks, or saving the
- * actions undertaken by the API to a logging file.
+ * persistence layer (DAO), meaning that the Service class performs tasks that aren't done through the web or
+ * SQL, such as programming tasks like checking that the input is valid, conducting additional security checks,
+ *  or saving the actions undertaken by the API to a logging file.
  *
  * It's perfectly normal to have Service methods that only contain a single line that calls a DAO method. An
  * application that follows best practices will often have unnecessary code, but this makes the code more
@@ -20,7 +20,6 @@ public class BookService {
 
     /**
      * No-args constructor for bookService which creates a BookDAO.
-     * There is no need to change this constructor.
      */
     public BookService(){
         bookDAO = new BookDAO();
@@ -29,7 +28,6 @@ public class BookService {
      * Constructor for a BookService when a BookDAO is provided.
      * This is used for when a mock BookDAO that exhibits mock behavior is used in the test cases.
      * This would allow the testing of BookService independently of BookDAO.
-     * There is no need to modify this constructor.
      * @param bookDAO
      */
     public BookService(BookDAO bookDAO){
@@ -44,8 +42,8 @@ public class BookService {
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
-     * An ISBN will be provided in Book. Method should check if the book ISBN already exists before it attempts to
-     * persist it.
+     * An ISBN will be provided in Book. Method should check if the book ISBN already exists before it attempts to persist it.
+     *
      * @param book a book object.
      * @return book if it was successfully persisted, null if it was not successfully persisted (eg if the book primary
      * key was already in use.)
