cc87c60361e0a369a22c8b90fa4baab3f0ec2d39
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-04-01 11:38:19 -0400

    Update BookDAO.java
    updated docs

--- src/main/java/Application/DAO/BookDAO.java
+++ src/main/java/Application/DAO/BookDAO.java
@@ -11,20 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A DAO is a class that mediates the transformation of data between the format of objects in Java to rows in a
- * database. The methods here are mostly filled out, you will just need to add a SQL statement.
+ * DAO class mediates the transformation of data between the format of objects in Java to rows in a database.
  *
- * We may assume that the database has already created a table named 'book'.
- * It contains similar values as the Author class:
- * isbn, which is of type int and is a primary key,
- * author_id, which is of type int, and is a foreign key associated with the column 'id' of 'author',
- * title, which is of type varchar(255),
- * copies_available, which is of type int.
+ * database has a table named 'book':  
+ *      isbn, which is of type int and is a primary key,
+ *      author_id, which is of type int, and is a foreign key associated with the column 'id' of 'author',
+ *      title, which is of type varchar(255),
+ *      copies_available, which is of type int.
  */
 public class BookDAO {
     /**
      * TODO: retrieve all books from the Book table.
-     * You only need to change the sql String.
      * @return all Books.
      */
     public List<Book> getAllBooks(){
@@ -50,17 +47,14 @@ public class BookDAO {
 
     /**
      * TODO: retrieve a book from the Book table, identified by its isbn.
-     * You only need to change the sql String and leverage PreparedStatement's setString and setInt methods.
      * @return a book identified by isbn.
      */
     public Book getBookByIsbn(int isbn){
         Connection connection = ConnectionUtil.getConnection();
         try {
-            //Write SQL logic here
             String sql = "SELECT * FROM book WHERE isbn = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-            //write preparedStatement's setInt method here.
             preparedStatement.setInt(1, isbn);
 
             ResultSet rs = preparedStatement.executeQuery();
@@ -79,19 +73,16 @@ public class BookDAO {
 
     /**
      * TODO: insert a book into the Book table.
-     * Unlike some of the other insert problems, the primary key here will be provided by the client as part of the
-     * Book object. Given the specific nature of an ISBN as both a numerical organization of books outside of this
-     * database, and as a primary key, it would make sense for the client to submit an ISBN when submitting a book.
-     * You only need to change the sql String and leverage PreparedStatement's setString and setInt methods.
+     * primary key here will be provided by the client as part of the Book object
+     * Given the specific nature of an ISBN as both a numerical organization of books outside of this db
+     * and as a primary key, it makes sense for the client to submit an ISBN when submitting a book.
      */
     public Book insertBook(Book book){
         Connection connection = ConnectionUtil.getConnection();
         try {
-            //Write SQL logic here
             String sql = "INSERT INTO book (isbn, author_id, title, copies_available) VALUES (?, ?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-            //write preparedStatement's setString and setInt methods here.
             preparedStatement.setInt(1, book.getIsbn());
             preparedStatement.setInt(2, book.getAuthor_id());
             preparedStatement.setString(3, book.getTitle());
@@ -106,7 +97,6 @@ public class BookDAO {
     }
     /**
      * TODO: retrieve all books from the Book table with copies_available over zero.
-     * You only need to change the sql String with a query that utilizes a WHERE clause.
      * @returnall books with book count > 0.
      */
     public List<Book> getBooksWithBookCountOverZero(){
